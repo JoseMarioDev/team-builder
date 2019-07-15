6937b0f95cd7560973835387f3bd89fe270a4584
Jose Montero Jr <josemariodev@gmail.com>
2019-07-15 16:40:08 -0500

    created hanlder functions, passed as props to form.js

--- team-builder-app/src/App.js
+++ team-builder-app/src/App.js
@@ -1,12 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 
 function App() {
   const [member, setMember] = useState({ name: '', email: '', role: '' });
+
+  const changeHandler = e => {
+    setMember({ ...member, [e.target.name]: e.target.value });
+  };
+  const submitHandler = e => {
+    e.preventDefault();
+    setMember({ name: '', email: '', role: '' });
+    console.log(member.name, member.email, member.role);
+  };
+
   return (
     <div className='App'>
-      <Form />
+      <Form
+        changeHandler={changeHandler}
+        submitHandler={submitHandler}
+        member={member}
+        setMember={setMember}
+      />
     </div>
   );
 }
